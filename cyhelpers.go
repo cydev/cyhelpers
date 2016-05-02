b695b36ecddc958d058e779c4fd2ae232ebb046e
Aleksandr Razumov <ar@cydev.ru>
2016-05-02 21:15:34 +0300

    init

--- cyhelpers.go
+++ cyhelpers.go
@@ -2,21 +2,54 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
+
+var names = [...]string{
+	"cydev",
+	"ernado",
+}
+
+func completion(prefix string) string {
+	candidates := []string{}
+	exists := make(map[string]bool)
+	for _, name := range names {
+		dir := filepath.Join(prefix, name)
+		dirs, err := ioutil.ReadDir(dir)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, d := range dirs {
+			if !d.IsDir() {
+				continue
+			}
+			candidate := d.Name()
+			if exists[candidate] {
+				candidate = filepath.Join(name, candidate)
+			}
+			exists[candidate] = true
+			candidates = append(candidates, candidate)
+		}
+	}
+	return strings.Join(candidates, " ")
+}
 
 func main() {
 	gopath, exists := os.LookupEnv("GOPATH")
 	if !exists {
 		gopath = "/go"
 	}
+	prefix := filepath.Join(gopath, "src", "github.com")
 	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Project name not provided")
-		os.Exit(-1)
+		// returning completion list
+		fmt.Print(completion(prefix))
+		os.Exit(1)
 	}
 	project := os.Args[1] // cydev/project, ernado/project or just project
-	prefix := filepath.Join(gopath, "src", "github.com")
 	candidates := []string{
 		project,
 		filepath.Join("cydev", project),
@@ -31,8 +64,7 @@ func main() {
 		}
 	}
 	if len(path) == 0 {
-		fmt.Fprintln(os.Stderr, "Candidate not found")
-		os.Exit(-2)
+		log.Fatal("project", project, "not found")
 	}
 	fmt.Print(path)
 }
--- cyhelpers.sh
+++ cyhelpers.sh
@@ -1,5 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+p_complete () {
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    subcommands="$(cyhelpers)"
 
+    if [[ ${COMP_CWORD} == 1 ]] ; then
+        COMPREPLY=($(compgen -W "${subcommands}" -- ${cur}))
+        return 0
+    fi
+}
 p () {
     cd $(cyhelpers $1)
 }
+
+complete -F p_complete p
