abb0478680fcdfd411bb04a0c7ccef50e56e0fe3
Aleksandr Razumov <ar@cydev.ru>
2016-05-04 16:34:24 +0300

    update cyhelpers to handle /src

--- cyhelpers.go
+++ cyhelpers.go
@@ -12,41 +12,54 @@ import (
 var names = [...]string{
 	"cydev",
 	"ernado",
+	".",
 }
 
-func completion(prefix string) string {
+var gopath string
+
+func completion(prefixes []string) string {
 	candidates := []string{}
 	exists := make(map[string]bool)
-	for _, name := range names {
-		dir := filepath.Join(prefix, name)
-		dirs, err := ioutil.ReadDir(dir)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, d := range dirs {
-			if !d.IsDir() {
+	for _, prefix := range prefixes {
+		for _, name := range names {
+			dir := filepath.Join(prefix, name)
+			dirs, err := ioutil.ReadDir(dir)
+			if err != nil {
 				continue
 			}
-			candidate := d.Name()
-			if exists[candidate] {
-				candidate = filepath.Join(name, candidate)
+			// do not complete all gopath
+			if (name == ".") && strings.HasPrefix(prefix, gopath) {
+				continue
 			}
-			exists[candidate] = true
-			candidates = append(candidates, candidate)
+			for _, d := range dirs {
+				if !d.IsDir() {
+					continue
+				}
+				candidate := d.Name()
+				if exists[candidate] {
+					candidate = filepath.Join(name, candidate)
+				}
+				exists[candidate] = true
+				candidates = append(candidates, candidate)
+			}
 		}
 	}
 	return strings.Join(candidates, " ")
 }
 
 func main() {
-	gopath, exists := os.LookupEnv("GOPATH")
-	if !exists {
+	var gopathExists bool
+	gopath, gopathExists = os.LookupEnv("GOPATH")
+	if !gopathExists {
 		gopath = "/go"
 	}
-	prefix := filepath.Join(gopath, "src", "github.com")
+	prefixes := []string{
+		filepath.Join(gopath, "src", "github.com"), // golang projects
+		"/src", // other projects
+	}
 	if len(os.Args) < 2 {
 		// returning completion list
-		fmt.Print(completion(prefix))
+		fmt.Print(completion(prefixes))
 		os.Exit(1)
 	}
 	project := os.Args[1] // cydev/project, ernado/project or just project
@@ -56,13 +69,16 @@ func main() {
 		filepath.Join("ernado", project),
 	}
 	var path string
-	for _, candidate := range candidates {
-		pathCandidate := filepath.Join(prefix, candidate)
-		if _, err := os.Stat(pathCandidate); err == nil {
-			path = pathCandidate
-			break
+	for _, prefix := range prefixes {
+		for _, candidate := range candidates {
+			pathCandidate := filepath.Join(prefix, candidate)
+			if _, err := os.Stat(pathCandidate); err == nil {
+				path = pathCandidate
+				break
+			}
 		}
 	}
+
 	if len(path) == 0 {
 		// trying to return CWD
 		dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
